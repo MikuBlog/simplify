61794e60eeb566d4f27ecd490097e4cadb64d78b
xuanzai <43233731+MikuBlog@users.noreply.github.com>
2019-06-30 22:10:07 +0800

    二次封装axios

--- axios/http.js
+++ axios/http.js
@@ -1,4 +1,59 @@
+	import Vue from 'vue'
 	import axios from 'axios'
+	import { Loading, Message } from 'element-ui'
+	import 'element-ui/lib/theme-chalk/index.css';
+
+	// 添加拦截提示	
+	function justifyStatus(result) {
+		switch(result) {
+			case 404: Message({
+				showClose: true,
+				message: "找不到该接口，请联系程序员进行修复",
+				type: 'error'
+			});break;
+			case 401: Message({
+				showClose: true,
+				message: "权限不足",
+				type: 'error'
+			});break;
+			case 0: Message({
+				showClose: true,
+				message: "请登录",
+				type: 'error'
+			});break;
+			case 500: 
+			default: Message({
+				showClose: true,
+				message: "服务器出错，请联系程序员进行修复",
+				type: 'error'
+			});
+		}
+	}
+	
+	// 添加拦截器函数
+	function addInterceptors(obj) {
+		obj.interceptors.request
+			.use(config => {
+				loading = Loading.service({ fullscreen: true })
+				return config
+			}, err => {
+				justifyStatus('none')
+				loading.close()
+				return Promise.reject(err)
+			})
+			
+		obj.interceptors.response
+			.use(response => {
+				loading.close()
+				return response;
+			}, err => {
+				justifyStatus(err.response.status)
+				loading.close()
+				return Promise.reject(err)
+			})
+	}
+	
+	let loading
 	
 	// 给所有的实例配置请求根路径
 	axios.defaults.baseURL = 'http://myinterface.xuanzai.top/'
@@ -15,7 +70,7 @@
 		}
 	}]
 	
-		// 发送数据格式为键值对
+	// 发送数据格式为键值对
 	const myAxios_1 = axios.create({
 		headers: {
 			'Content-Type': 'application/x-www-form-urlencoded'
@@ -53,8 +108,16 @@
 		}],
 	})
 	
+	// 添加拦截器
+	addInterceptors(myAxios_1)
+	
+	addInterceptors(myAxios_2)
+	
+	addInterceptors(myAxios_3)
+	
 	export default {
 		myAxios_1,
 		myAxios_2,
 		myAxios_3
 	}
+
