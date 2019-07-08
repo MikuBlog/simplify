02b32395b802600126ca32aa9b6ca0e849f2d306
MikuBlog <786601672@qq.com>
2019-07-08 21:13:37 +0800

    添加存储模块

---
+++ plugins/storage/storage.js
@@ -0,0 +1,42 @@
+/**
+ * 保存会话数据
+ * @param {String} key 
+ * @param {String | Object | Number | Boolean} data 
+ */
+function setMemorySes(key, data) {
+    sessionStorage.setItem(key, JSON.stringify(data))
+}
+
+/**
+ * 获取会话数据
+ * @param {String} key 
+ * @returns {String | Object | Number | Boolean}
+ */
+function getMemorySes(key) {
+    return JSON.parse(sessionStorage.getItem(key))
+}
+
+/**
+ * 长久保存数据
+ * @param {String} key 
+ * @param {String | Object | Number | Boolean} data 
+ */
+function setMemoryPmt(key, data) {
+    localStorage.setItem(key, JSON.stringify(data))
+}
+
+/**
+ * 获取长久数据
+ * @param {String} key 
+ * @returns {String | Object | Number | Boolean}
+ */
+function getMemoryPmt(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+export default {
+    setMemorySes,
+    getMemorySes,
+    setMemoryPmt,
+    getMemoryPmt
+}
